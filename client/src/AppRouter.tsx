4950fdff44b5b71dcb3bdc4c4937b9bc7071e12f
chneau <charles63500@gmail.com>
2024-06-27 23:31:39 +0100

    chore: Add @tanstack/react-router and @tanstack/router-devtools dependencies

---
+++ client/src/AppRouter.tsx
@@ -0,0 +1,59 @@
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+import {
+	Link,
+	Outlet,
+	RouterProvider,
+	createRootRoute,
+	createRoute,
+	createRouter,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { App } from "./App";
+import { queryClient } from "./queryClient";
+
+const localStoragePersister = createSyncStoragePersister({
+	storage: window.localStorage,
+});
+persistQueryClient({ queryClient, persister: localStoragePersister });
+const rootRoute = createRootRoute({
+	component: () => (
+		<QueryClientProvider client={queryClient}>
+			<div>
+				<Link to="/">Home</Link> <Link to="/about">About</Link>
+			</div>
+			<hr />
+			<Outlet />
+			<TanStackRouterDevtools />
+			<ReactQueryDevtools />
+		</QueryClientProvider>
+	),
+});
+
+const indexRoute = createRoute({
+	getParentRoute: () => rootRoute,
+	path: "/",
+	component: App,
+});
+
+const aboutRoute = createRoute({
+	getParentRoute: () => rootRoute,
+	path: "/about",
+	component: function About() {
+		return <>Hello from About!</>;
+	},
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+export const AppRouter = () => <RouterProvider router={router} />;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,11 @@
-import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { persistQueryClient } from "@tanstack/react-query-persist-client";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
-import { queryClient } from "./queryClient";
-
-const localStoragePersister = createSyncStoragePersister({
-	storage: window.localStorage,
-});
-persistQueryClient({ queryClient, persister: localStoragePersister });
+import { AppRouter } from "./AppRouter";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Root element not found");
 createRoot(root).render(
-	<QueryClientProvider client={queryClient}>
-		<ReactQueryDevtools initialIsOpen={false} />
-		<StrictMode>
-			<App />
-		</StrictMode>
-	</QueryClientProvider>,
+	<StrictMode>
+		<AppRouter />
+	</StrictMode>,
 );
