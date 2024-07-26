1c5f7c355c3a9abba316ac76765dc5d1dc49d73b
chneau <charles63500@gmail.com>
2024-07-26 15:57:32 +0100

    refactor: Update server.ts to use @chneau/elysia-compression for improved response compression

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,9 @@
+import { compression } from "@chneau/elysia-compression";
 import cors from "@elysiajs/cors";
 import jwt from "@elysiajs/jwt";
 import { serverTiming } from "@elysiajs/server-timing";
 import { PrismaClient } from "@prisma/client";
 import { Elysia, error, t } from "elysia";
-import { compression } from "./compression";
 import { logging } from "./logging";
 import { PostInputCreate, PostInputUpdate } from "./prisma/prismabox/Post";
 
